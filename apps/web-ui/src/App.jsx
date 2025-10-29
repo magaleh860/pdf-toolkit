81e063cff59f296c7708a2f8c7dd355c6e6ca0c3
Mohammed A Morshid <magaleh860@gmail.com>
2025-10-29 05:01:01 -0400

    added about page

--- apps/web-ui/src/App.jsx
+++ apps/web-ui/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, NavLink, useLocation } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, NavLink, useLocation, Link } from 'react-router-dom'
 import { MergePage } from './pages/MergePage'
 import { SplitPage } from './pages/SplitPage'
 import { EditPage } from './pages/EditPage'
 import { LandingPage } from './pages/LandingPage'
+import { AboutPage } from './pages/AboutPage'
 import { ThemeToggle } from './components/ThemeToggle'
 import { Footer } from './components/Footer'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import './styles/landing.css'
 import './styles/edit.css'
+import './styles/about.css'
 
 // Set worker source to the bundled worker
 import * as pdfjsLib from 'pdfjs-dist/build/pdf.min.mjs'
@@ -49,7 +53,12 @@ export default function App() {
       <div className="app">
         <header className="app-header">
           <h1>PDF Toolkit</h1>
-          <ThemeToggle />
+          <div className="header-actions">
+            <Link to="/about" className="about-nav-link">
+              <FontAwesomeIcon icon={faInfoCircle} /> About
+            </Link>
+            <ThemeToggle />
+          </div>
         </header>
         <Navigation />
 
@@ -58,6 +67,7 @@ export default function App() {
           <Route path="/merge" element={<MergePage />} />
           <Route path="/split" element={<SplitPage />} />
           <Route path="/edit" element={<EditPage />} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
 
         <Footer />
