3b9c7fbe215b505a82b7b8a74a78e30549444fe8
Mohammed A Morshid <magaleh860@gmail.com>
2025-10-29 00:59:32 -0400

    Added text to landing page and footer

--- apps/web-ui/src/main.jsx
+++ apps/web-ui/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { ThemeProvider } from './hooks/useTheme.jsx'
-import './styles.css'
+import './styles/styles.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
